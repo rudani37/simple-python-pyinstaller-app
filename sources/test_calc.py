81947b10cb441555015ebb6c507a29ed5a2c81ee
Rudani <rudani097@gmail.com>
2022-10-24 14:55:11 +0700

    Update test_calc.py

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -1,10 +1,5 @@
 import unittest
 import calc
-import os
-
-app = Flask(__name__)
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port, debug=True)
 
 class TestCalc(unittest.TestCase):
     """
